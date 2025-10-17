86881b2c17617657f700efcf04936f8a7a532efd
donjizzkan <donjizzkan@gmail.com>
2025-10-17 16:52:37 +0900

    week 3

--- src/player.hpp
+++ src/player.hpp
@@ -2,7 +2,9 @@
 // player 데이터 저장
 // =====================
 #include <iostream>
-
+#include <array>
+#include "SFML/System.hpp"
+#include "application.hpp"
 using namespace std;
 
 // 절대적인 수치를 저장
@@ -17,4 +19,21 @@ public:
 struct PlayerStatus{
     int nowHp;
     int nowStamina;
+    array<StatusEffectDetail, (size_t)StatusEffect::cnt> effects;
 };
+
+// 상태이상 효과 종류들
+enum class StatusEffect{
+    Stun,
+    Poison,
+    Burn,
+    cnt                 // 상태이상 효과의 총 개수 세는용
+};
+
+// 각 상태이상의 디테일저장용
+struct StatusEffectDetail{
+    StatusEffect effect;
+    int effectTurn;     
+    int effectDmg;
+};
+
